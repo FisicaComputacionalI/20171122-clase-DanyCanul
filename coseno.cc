709ed31fd5aa908bc89bb54ebc2665cb0847d56f
DanyCanul <31377538+DanyCanul@users.noreply.github.com>
2017-11-22 13:26:28 -0600

    Add files via upload
    Coseno

---
+++ coseno.cc
@@ -0,0 +1,15 @@
+//Programa
+//Autor: Daniela Canul Garcia
+//Fecha: 22 de Noviembre del 2017
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+cout << "Introduce el angulo:";
+float x;
+cin >> x;
+float coseno=cos(x);
+cout << "El coseno es " << coseno << endl;
+return 0;
+}
+
